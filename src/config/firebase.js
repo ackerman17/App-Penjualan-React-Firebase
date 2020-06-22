4945d951605128f321bdbbfc481fc45f3cbcaa65
mhafet17 <do.crazy192@gmail.com>
2020-06-23 05:43:52 +0700

    Mempersiapkan Config FirebaseProvider

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAAa7K4AWUb2K6ODvd5xXOAUSTZ90QMsmU",
+    authDomain: "react-shop-hafidz.firebaseapp.com",
+    databaseURL: "https://react-shop-hafidz.firebaseio.com",
+    projectId: "react-shop-hafidz",
+    storageBucket: "react-shop-hafidz.appspot.com",
+    messagingSenderId: "95369111097",
+    appId: "1:95369111097:web:b4c1d90a9c0544a6fd8fe8",
+    measurementId: "G-CDDGZ8ZQTW"
+};
+
+export default firebaseConfig;
