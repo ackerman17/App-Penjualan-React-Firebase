4eb0d6988abca5417752c82da2c02e0771037df5
mhafet17 <do.crazy192@gmail.com>
2020-07-22 15:30:33 +0700

    add button sign out in index

--- src/pages/private/home/index.js
+++ src/pages/private/home/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Button from '@material-ui/core/Button';
+import { useFirebase } from '../../../components/FirebaseProvider';
 
-
-export default function Home() {
+export default function Home () {
+    const { auth } = useFirebase();
     return (
-        <h1>Halaman Home</h1>
+        <>
+            <h1>Halaman Home</h1>
+            <Button onClick={(e) => {
+                auth.signOut()
+            }}>Sign Out</Button>
+        </>
     )
 }
